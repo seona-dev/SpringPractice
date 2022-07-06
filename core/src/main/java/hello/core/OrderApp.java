15976313696c4ae24117dae8801c0154d3828d61
seona-dev <seona0826@gmail.com>
2022-07-07 02:58:57 +0900

    create OrderApp class

---
+++ core/src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,9 @@
+package hello.core;
+
+import hello.core.member.MemberServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        new MemberServiceImpl();
+    }
+}
